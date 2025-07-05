ac7e94e70dc69461227857940074759f1649ce7e
Eduardo-Lima-Dev <limadudu34@gmail.com>
2025-07-05 09:56:08 -0300

    ✨ refactor(api): remover a dependência da variável baseUrl na função getProdutos, utilizando a URL relativa diretamente

--- Codigos/malibu-atelie/src/lib/api.ts
+++ Codigos/malibu-atelie/src/lib/api.ts
@@ -1,7 +1,5 @@
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
- 
 export async function getProdutos() {
-  const res = await fetch(`${baseUrl}/api/products`, { cache: "no-store" });
+  const res = await fetch(`/api/products`, { cache: "no-store" });
   if (!res.ok) return [];
   return res.json();
 } 
